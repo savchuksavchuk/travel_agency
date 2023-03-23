51cc7bd2610fdf0a173acfb4cc5d40dc99ac3037
Oleh Savchuk <sav4uk.oleg@gmail.com>
2023-03-24 01:03:43 +0200

    mobile added

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import './src/styles/main.scss';
 import { initPickGallery } from './src/scripts/initPickGallery';
 import { initSpotsSlider } from './src/scripts/initSpotsSlider';
+import { navMobile } from './src/scripts/navMobile';
+import { submitForm } from './src/scripts/submitForm';
 
 initPickGallery();
 initSpotsSlider();
+navMobile();
+submitForm();
